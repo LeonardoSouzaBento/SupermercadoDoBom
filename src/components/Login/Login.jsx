ace6e5e04b9ae2ef2f69c04f0547c003b19596ad
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2025-08-10 13:43:42 -0300

    correção dos dados do firebaseConfig

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -16,6 +16,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { VisibilityContext } from "../../contexts/VisibilityContext";
 
+/*dados do firebaseConfig Corrijidos*/
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
