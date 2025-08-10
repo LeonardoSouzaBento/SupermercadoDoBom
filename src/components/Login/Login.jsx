af70e6a1d7e94b70b61a22b7fd5ed72e9fa36dff
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2025-08-10 16:21:58 -0300

    teste

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -18,6 +18,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+/**/
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
