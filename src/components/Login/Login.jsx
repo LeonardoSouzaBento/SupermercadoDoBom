7c37e6a77f4f5bc115c9ff9a832ed8598e4eeabb
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2025-08-10 13:14:55 -0300

    Correção de confitos no repositorio

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -16,7 +16,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { VisibilityContext } from "../../contexts/VisibilityContext";
 
-/**/
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
