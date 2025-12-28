d6684589750278f40a22352d2464cd6d84efcfaa
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2025-12-28 14:37:50 -0300

    fix(correção do arquivo index)

--- src/pages/Cart/Components/index.js
+++ src/pages/Cart/Components/index.js
@@ -1,6 +1,6 @@
-export { default as CancelDialog } from './cancel-dialog.jsx';
-export { default as ButtonSeeAll } from './button-see-all.jsx';
-export { default as DetailSection } from './detail-section.jsx';
-export { default as ReceiptAndContinueSection } from './continue-section.jsx';
-export { default as ProductCardHeader } from './product-card-header.jsx';
+export { default as CancelDialog } from './cancel-dialog';
+export { default as ButtonSeeAll } from './button-see-all';
+export { default as DetailSection } from './detail-section';
+export { default as ReceiptAndContinueSection } from './continue-section';
+export { default as ProductCardHeader } from './product-card-header';
 
