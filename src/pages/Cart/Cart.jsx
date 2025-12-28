4761b63d48ad6232ba50fc5d74283a9697dc2e23
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2025-12-28 11:52:05 -0300

    fix(correçao de caminho de arquivo).

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -6,7 +6,7 @@ import { CartContext } from '@contexts/CartContext.js';
 import { VisibilityContext } from '@contexts/VisibilityContext.js';
 import { useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ButtonSeeAll, CancelDialog, DetailSection, ReceiptAndContinueSection, ProductCardHeader } from './components';
+import { ButtonSeeAll, CancelDialog, DetailSection, ReceiptAndContinueSection, ProductCardHeader } from './components/index';
 import { CartSectionStyed, MainWrapperStyled } from './ui/index';
 
 //altura - o cabeçalho 'sua compra'
